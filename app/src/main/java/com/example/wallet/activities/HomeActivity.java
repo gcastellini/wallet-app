c1214931e5923f6c386136181a6bfcefb3ee935e
Giuliana Castellini <giuliana_castellini@hotmail.com>
2023-11-17 01:47:08 -0300

    add menu action

--- app/src/main/java/com/example/wallet/activities/HomeActivity.java
+++ app/src/main/java/com/example/wallet/activities/HomeActivity.java
@@ -2,9 +2,14 @@ package com.example.wallet.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.wallet.R;
 import com.example.wallet.adapters.MyPagerAdapter;
@@ -15,15 +20,40 @@ public class HomeActivity extends AppCompatActivity {
 
     private final String [] data ={"Dashboards","Wallets","Info"};
 
+    private MenuItem item;
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.list,menu);
+        item = menu.findItem(R.id.add_wallet);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         ViewPager2 viewPager = findViewById(R.id.pager);
         viewPager.setAdapter(
                 new MyPagerAdapter(this)
         );
+
+
 
         TabLayout tabsLayout= findViewById(R.id.tab_layout);
         new TabLayoutMediator(
@@ -39,7 +69,24 @@ public class HomeActivity extends AppCompatActivity {
 
         ).attach();
 
+        tabsLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                item.setVisible(tab.getPosition() == 1);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
 
 
         }
+
 }
